b5f792f982acf0aa38ce759a50c0a6479e3a43f4
will-206 <will9_30@yahoo.com>
2017-02-15 16:24:33 -0800

    add unit, merc filters

--- src/App.js
+++ src/App.js
@@ -9,24 +9,38 @@ class App extends Component {
       soulstones: 50,
       soulstonesSpent: 0,
       faction: 'Select',
-      leader: 'Select',
-      cache: 0
+      leader: {}
     }
     this.onDropDownChange = this.onDropDownChange.bind(this);
     this.onSSChange = this.onSSChange.bind(this);
     this.checkLeader = this.checkLeader.bind(this);
+    this.checkUpgrade = this.checkUpgrade.bind(this);
     this.compare = this.compare.bind(this);
+    this.onUpgradeChange = this.onUpgradeChange.bind(this);
+    this.checkUnit = this.checkUnit.bind(this);
+    this.checkMerc = this.checkMerc.bind(this);
+
   }
 
   onDropDownChange(event){
     if (event.target.name === 'faction'){
-      this.setState({leader: 'select'})
-      this.setState({[event.target.name]: event.target.value})
+      this.setState({
+        leader: {},
+        faction: event.target.value
+      })
     } else {
-      this.setState({[event.target.name]: event.target.value})
+      console.log(event.target.value);
+      this.setState({leader: data[event.target.value]})
     }
   }
 
+  onUpgradeChange(event){
+
+  }
+
+  onUnitChange(event){
+
+  }
   onSSChange(event){
     this.setState({soulstones: event.target.value})
   }
@@ -47,6 +61,39 @@ class App extends Component {
     return master || henchman;
   }
 
+  checkUpgrade(unit){
+    if (unit.characteristics.includes('Campaign')) {
+      return false;
+    }
+    if (!this.compare(unit.faction, this.state.faction) || !this.compare(unit.rank, 'Upgrade')){
+      return false;
+    }
+    console.log(unit);
+    return true;
+  }
+
+  checkUnit(unit){
+    if(!this.compare(unit.faction, this.state.faction) || this.compare(unit.rank, 'Upgrade') || this.compare(unit.rank, 'Master')){
+      return false;
+    }
+    if(this.compare(unit.name, this.state.leader.name)){
+      return false;
+    }
+    return true;
+  }
+
+  checkMerc(unit){
+    if(!this.compare(unit.characteristics, 'Mercenary')){
+      return false;
+    }
+    if(this.compare(unit.faction, this.state.faction)){
+      return false;
+    }
+    if(this.state.faction === 'Guild' && unit.name === 'Lazarus'){
+      return false;
+    }
+    return true;
+  }
   compare(a, b) {
     return (a.includes(b) || a === b);
   }
@@ -81,22 +128,69 @@ class App extends Component {
 
         <div>
           Leader
-          <select name="leader" value={this.state.leader} onChange={this.onDropDownChange}>
-            <option disabled>Select
+          <select name="leader"  onChange={this.onDropDownChange}>
+            <option>Select
             </option>
             {
               data.filter(this.checkLeader).map(el => (
-                <option key ={el.id}value={el.name}>{el.name}
+                <option key={el.id} value={el.id}>{el.name}
                 </option>
                 )
               )
             }
           </select>
-          {/* <div>+ upgrade</div> */}
+          <div>Cache {this.state.leader.cache}</div>
+          <div>
+            <button>
+              + add upgrade
+            </button>
+            <select name="upgrade"  onChange={this.onUpgradeChange}>
+              <option>Select
+              </option>
+              {
+                data.filter(this.checkUpgrade).map(el => (
+                  <option key={el.id} value={el.id}>{el.name}  - {el.cost}ss
+                  </option>
+                  )
+                )
+              }
+            </select>
+          </div>
         </div>
 
-        <div>Cache {this.state.cache}</div>
-        <div>+ add unit</div>
+        <div>
+          <button>
+            + add unit
+          </button>
+          <select name="unit"  onChange={this.onUnitChange}>
+            <option>Select
+            </option>
+            {
+              data.filter(this.checkUnit).map(el => (
+                <option key={el.id} value={el.id}>{el.name}  - {el.cost}ss
+                </option>
+                )
+              )
+            }
+          </select>
+        </div>
+
+        <div>
+          <button>
+            + add merc
+          </button>
+          <select name="unit"  onChange={this.onUnitChange}>
+            <option>Select
+            </option>
+            {
+              data.filter(this.checkMerc).map(el => (
+                <option key={el.id} value={el.id}>{el.name}  - {el.cost + 1}ss
+                </option>
+                )
+              )
+            }
+          </select>
+        </div>
       </div>
     );
   }
